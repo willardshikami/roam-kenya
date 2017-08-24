67837311b47837f303f5dd052b48b3ef964c9fcd
willardshikami <willardshikami@gmail.com>
2017-08-24 23:02:55 +0300

    added reducerslist to form one reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,18 @@
 import {combineReducers} from 'redux';
 import CityReducer from './reducer-cities';
+import HotelReducer from './reducer-hotels';
+import ActivityReducer from './reducer-activites';
+import AttractionReducer from './reducer-attractions';
+import EateryReducer from './reducer-eatery';
+import SurvivalReducer from './reducer-survival';
 
  const allReducers = combineReducers({
-     cities : CityReducer
+    activities: ActivityReducer, 
+    attractions: AttractionReducer,
+    cities: CityReducer,
+    eateries: EateryReducer,
+    hotels: HotelReducer,
+    survivals: SurvivalReducer 
  });
 
  export default allReducers;
