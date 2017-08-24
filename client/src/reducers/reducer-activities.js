0a83924bf14af0d40d8fb9ae971cd624515b786d
willardshikami <willardshikami@gmail.com>
2017-08-24 22:42:51 +0300

    some more reducers

---
+++ client/src/reducers/reducer-activities.js
@@ -0,0 +1,15 @@
+export default function(){
+    return[
+        {
+            id: 1,
+            name: "",
+            description: "",
+            duration: "",
+            included: "",
+            accomodations: "",
+            meals: "",
+            image_url: "",
+            location: ""
+        }
+    ]
+}
---
+++ client/src/reducers/reducer-hotels.js
@@ -0,0 +1,18 @@
+export default function(){
+    return[
+        {
+            id: 1,
+            name: "",
+            description: "",
+            convinence: "",
+            contact: "",
+            rating: "",
+            situate: "",
+            meals: "",
+            property_description: "",
+            important_info: "",
+            image_url: "",
+            location: ""
+        }
+    ]
+}
