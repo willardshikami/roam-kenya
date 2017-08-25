2d4f3d44c4b19ffdfc7d55499a567f8d3a84ce74
willardshikami <willardshikami@gmail.com>
2017-08-25 03:05:01 +0300

    building up the hero component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import App from '../css/App.css';
-import CityList from '../containers/cities-list'
+import Homestyle from '../css/Homestyle.css';
+import hero from '../img/kenya_hero.jpg';
+import CityList from '../containers/cities-list';
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
-        
+        <div className="hero">
+            <img src={hero} alt={"Hero"}/>
+            <div className="hero_content">
+              <h1>KARIBU KENYA</h1>
+              <p>The Pride of Africa</p>
+            </div>
+        </div>
       </div>
     );
   }
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import App from '../css/App.css';
-import CityList from '../containers/cities-list'
+import CityList from '../containers/cities-list';
+import Home from '../components/Home.js';
 
 class Main extends Component {
   render() {
     return (
       <div className="Main">
-        
+        <Home/>
       </div>
     );
   }
