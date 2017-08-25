3f403606c891a44bede204bf58796717f0b99862
willardshikami <willardshikami@gmail.com>
2017-08-25 03:37:45 +0300

    updated home component with overlay

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import Homestyle from '../css/Homestyle.css';
-import hero from '../img/kenya_hero.jpg';
+import heroImage from '../img/hero_full.jpg';
 import CityList from '../containers/cities-list';
+
+
+const heroStyle = {
+  backgroundImage: "url(" + heroImage + ")",
+  height: "580px",
+}
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <div className="hero">
-            <img src={hero} alt={"Hero"}/>
+        <div className="hero" style={heroStyle}>
             <div className="hero_content">
               <h1>KARIBU KENYA</h1>
               <p>The Pride of Africa</p>
